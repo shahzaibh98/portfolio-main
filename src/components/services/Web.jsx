64f5cd8aee6815e61ede55575c3393c4b3dc8730
Shahzaib Hassan/Software Engineer/ISB <S.Hassan@sybrid.local>
2023-03-21 01:09:16 +0500

    20/3/2023 changes are done in my personal portfolio

--- src/components/services/Web.jsx
+++ src/components/services/Web.jsx
@@ -1,43 +1,49 @@
-import React from 'react'
-import {AiFillCloseCircle,AiFillCheckCircle} from 'react-icons/ai'
+import React from "react";
+import { AiFillCloseCircle, AiFillCheckCircle } from "react-icons/ai";
 
-const Web = ({open,onClose}) => {
-    if (!open) return null;
+const Web = ({ open, onClose }) => {
+  if (!open) return null;
   return (
     <div onClick={onClose} className="services_modal">
-    <div onClick={(x) => x.stopPropagation()} className="modal_content">
-      <i onClick={onClose} className="modal_close"><AiFillCloseCircle/></i>
+      <div onClick={(x) => x.stopPropagation()} className="modal_content">
+        <i onClick={onClose} className="modal_close">
+          <AiFillCloseCircle />
+        </i>
 
-      <h3 className="modal_title">Web Designer</h3>
-      <p className="modal_description">
-        Providing quality work to clients and companies.
-      </p>
+        <h3 className="modal_title">Web Designer</h3>
+        <p className="modal_description">
+          Providing quality work to clients and companies.
+        </p>
 
-      <ul className="modal_list">
-        <li className="modal_item">
-          <i className='modal_icon'><AiFillCheckCircle/></i>
-          <p className="modal_info">
-            Creation, design and maintenance of websites
-          </p>
-        </li>
+        <ul className="modal_list">
+          <li className="modal_item">
+            <i className="modal_icon">
+              <AiFillCheckCircle />
+            </i>
+            <p className="modal_info">
+              Creation, design and maintenance of websites
+            </p>
+          </li>
 
-        <li className="modal_item">
-          <i className='modal_icon'><AiFillCheckCircle/></i>
-          <p className="modal_info">
-            Realizing the client's dream web site design
-          </p>
-        </li>
+          <li className="modal_item">
+            <i className="modal_icon">
+              <AiFillCheckCircle />
+            </i>
+            <p className="modal_info">
+              Realizing the client's dream web site design
+            </p>
+          </li>
 
-        <li className="modal_item">
-          <i className='modal_icon'><AiFillCheckCircle/></i>
-          <p className="modal_info">
-            Quality and strict work
-          </p>
-        </li>
-      </ul>
+          <li className="modal_item">
+            <i className="modal_icon">
+              <AiFillCheckCircle />
+            </i>
+            <p className="modal_info">Quality and strict work</p>
+          </li>
+        </ul>
+      </div>
     </div>
-  </div>
-  )
-}
+  );
+};
 
-export default Web
+export default Web;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
-import ReactDOM from 'react-dom/client'
-import React from 'react'
-import {App} from './App'
-import './index.css'
+import ReactDOM from "react-dom/client";
+import React from "react";
+import { App } from "./App";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<React.StrictMode><App/></React.StrictMode>);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
