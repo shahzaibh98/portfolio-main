3ab13d8d767dfe62b821403810fbccf96b393b23
Shahzaib Hassan/Software Engineer/ISB <S.Hassan@sybrid.local>
2023-03-21 17:15:48 +0500

    dark and light mode in my personal portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,51 @@
-import React, { useState } from 'react'
-import About from './components/about/about'
-import Contact from './components/contact/contact'
-import Education from './components/education/education'
-import Footer from './components/footer/footer'
-import Header from './components/header/header'
-import Navbar from './components/navbar/navbar'
-import Projects from './components/projects/projects'
-import Services from './components/services/services'
+import React, { useState } from "react";
+import About from "./components/about/about";
+import Contact from "./components/contact/contact";
+import Cursor from "./components/cursor/cursor";
+import Education from "./components/education/education";
+import Footer from "./components/footer/footer";
+import Header from "./components/header/header";
+import Navbar from "./components/navbar/navbar";
+import Projects from "./components/projects/projects";
+import Services from "./components/services/services";
+import "./styles.css";
+import { CiSun } from "react-icons/ci";
+import { RiMoonFoggyLine } from "react-icons/ri";
 
-import Cursor from './components/cursor/cursor'
-
-import { motion } from 'framer-motion'
+import { motion } from "framer-motion";
 
 export const App = () => {
-
-  const[theme, setTheme] = useState("dark");
+  const [theme, setTheme] = useState("dark");
   const changeTheme = () => {
     theme === "dark" ? setTheme("light") : setTheme("dark");
   };
 
   return (
-    <motion.div initial="hidden" animate="show" className='main app-container' data-theme = {theme}>
-      <Cursor/>
-      <Header/>
-      <Navbar/>
-      <About/>
-      <Education/>
-      <Services/>
-      <Projects/>
-      <Contact changeTheme = {changeTheme} currentTheme = {theme}/>
-      <Footer/>
+    <motion.div
+      initial="hidden"
+      animate="show"
+      className="main app-container"
+      data-theme={theme}
+    >
+      {theme !== "dark" ? (
+        <button className="fabDark" onClick={() => changeTheme()}>
+          <CiSun size={36} />
+        </button>
+      ) : (
+        <button className="fab" onClick={() => changeTheme()}>
+          <RiMoonFoggyLine size={36} />
+        </button>
+      )}
+
+      <Cursor />
+      <Header />
+      <Navbar />
+      <About />
+      <Education />
+      <Services />
+      <Projects />
+      <Contact changeTheme={changeTheme} currentTheme={theme} />
+      <Footer />
     </motion.div>
-  )
-}
+  );
+};
--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -1,25 +1,27 @@
-import React from 'react'
-import './contact.css'
+import React from "react";
+import "./contact.css";
 
-import {MdSettingsPower} from 'react-icons/md'
-import {LinkedinLogo, GitlabLogo, InstagramLogo} from 'phosphor-react'
+import { MdSettingsPower } from "react-icons/md";
+import { LinkedinLogo, GitlabLogo, InstagramLogo } from "phosphor-react";
 
-import { motion } from 'framer-motion'
-import { useScroll } from '../anima/useScroll'
-import {contact} from '../anima/animation'
+import { motion } from "framer-motion";
+import { useScroll } from "../anima/useScroll";
+import { contact } from "../anima/animation";
 
-const Contact = ({changeTheme, currentTheme}) => {
+const Contact = ({ changeTheme, currentTheme }) => {
   const [element, controls] = useScroll();
-  
+
   return (
     <section id="contact" className="contact">
-      <h2 className='contact_title'>Contact Me</h2>
-      <h5 className='contact_subtitle'>Get In Touch</h5>
-      <motion.div className="contacts container grid" ref={element}
-      variants={contact}
-      animate={controls}
-      transition={{delay: .6, duration: .9, type: "tween"}}>
-
+      <h2 className="contact_title">Contact Me</h2>
+      <h5 className="contact_subtitle">Get In Touch</h5>
+      <motion.div
+        className="contacts container grid"
+        ref={element}
+        variants={contact}
+        animate={controls}
+        transition={{ delay: 0.6, duration: 0.9, type: "tween" }}
+      >
         <div className="con_card">
           <div className="con_content">
             <div className="front">
@@ -27,7 +29,15 @@ const Contact = ({changeTheme, currentTheme}) => {
               <p className="con_subtitle">Hover me !</p>
             </div>
             <div className="back">
-              <p className="con_description"><a href="https://www.linkedin.com/in/erkan-uz-lil/" target="_blank" rel="noopener noreferrer"><LinkedinLogo size={80} className="con_so_icon"/></a></p>
+              <p className="con_description">
+                <a
+                  href="https://www.linkedin.com/in/erkan-uz-lil/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <LinkedinLogo size={80} className="con_so_icon" />
+                </a>
+              </p>
             </div>
           </div>
         </div>
@@ -39,7 +49,15 @@ const Contact = ({changeTheme, currentTheme}) => {
               <p className="con_subtitle">Hover me !</p>
             </div>
             <div className="back">
-              <p className="con_description"><a href="https://github.com/erkanuz" target="_blank" rel='noopener noreferrer'><GitlabLogo size={80} className="con_so_icon"/></a></p>
+              <p className="con_description">
+                <a
+                  href="https://github.com/erkanuz"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <GitlabLogo size={80} className="con_so_icon" />
+                </a>
+              </p>
             </div>
           </div>
         </div>
@@ -51,18 +69,21 @@ const Contact = ({changeTheme, currentTheme}) => {
               <p className="con_subtitle">Hover me !</p>
             </div>
             <div className="back">
-              <p className="con_description"><a href="https://www.instagram.com/" target="_blank" rel='noopener noreferrer'><InstagramLogo size={80} className="con_so_icon"/></a></p>
+              <p className="con_description">
+                <a
+                  href="https://www.instagram.com/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <InstagramLogo size={80} className="con_so_icon" />
+                </a>
+              </p>
             </div>
           </div>
         </div>
-
       </motion.div>
-      <div className='apple'>
-        <div onClick = {changeTheme}>{currentTheme === "dark" ? <MdSettingsPower className="light logo"/> : <MdSettingsPower className="dark logo" />}</div>
-        <h1 className='text1'>Dark Mode</h1>
-      </div>
     </section>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
