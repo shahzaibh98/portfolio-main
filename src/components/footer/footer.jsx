f3d6d22f4b3cd9484e3e901fc74eddd1c66695a5
Shahzaib Hassan/Software Engineer/ISB <S.Hassan@sybrid.local>
2023-03-22 19:14:34 +0500

    animations is added in my perosnal project

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -5,14 +5,6 @@ const footer = () => {
   return (
     <section id="footer" className="footer">
       <div className="footer_container container grid">
-        <div className="wrapper">
-          <div className="circle"></div>
-          <div className="circle"></div>
-          <div className="circle"></div>
-          <div className="shadow"></div>
-          <div className="shadow"></div>
-          <div className="shadow"></div>
-        </div>
         <div className="fline"></div>
         <div className="owner">
           <span className="footer_copy1 c">
